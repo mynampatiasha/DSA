fa1f0c36f4c956d698f0baabee8bec67b949ead2
Mynampati Asha <ashamynampati2003@gmail.com>
2025-08-01 22:58:01 +0530

    Update SlidingWindow1.py

--- Arrays/SlidingWindow1.py
+++ Arrays/SlidingWindow1.py
@@ -3,13 +3,10 @@ def max_sum_subarray(nums, k):
     window_sum = 0
     for i in range(k):
         window_sum += nums[i]
-    
     max_sum = window_sum
-
     # Slide the window over the rest of the array
     for i in range(k, len(nums)):
         window_sum = window_sum + nums[i] - nums[i - k]  # Slide the window 
         if window_sum > max_sum:
             max_sum = window_sum
-
     return max_sum
